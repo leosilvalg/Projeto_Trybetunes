3fbcc22857ad92ebac9206076ff52a2a96313416
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-03-07 18:29:47 -0300

    Refatorando o requisito 1 com base em uma aula sobre o conteúdo. Criei um componente apenas para armazenar as páginas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Login from './pages/Login';
-import Search from './pages/Search';
-import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
-import NotFound from './pages/NotFound';
+import Content from './Components/Content';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Login exact path="/" component={ Login } />
-        <Search path="/search" component={ Search } />
-        <Album path="/album/:id" component={ Album } />
-        <Favorites path="/favorites" component={ Favorites } />
-        <Profile path="/profile" component={ Profile } />
-        <ProfileEdit path="/profile/edit" component={ ProfileEdit } />
-        <NotFound path="*" component={ NotFound } />
+        <Content />
       </BrowserRouter>
     );
   }
