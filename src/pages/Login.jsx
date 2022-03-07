c77f9add1221321c892ea6bb8b0948cb163a032b
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-03-07 20:57:43 -0300

    Requisito 02. Chamando a função que faz a chamada a API

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../Components/Loading';
+import { createUser } from '../services/userAPI';
 
+const MAX_NAME_LENGTH = 3;
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      isLoading: false,
+      redirect: false,
+    };
+
+    this.submitButton = this.submitButton.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({ name: value });
+  }
+
+  async submitButton() {
+    const { name } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name });
+    this.setState({
+      isLoading: false,
+      redirect: true,
+    });
+  }
+
   render() {
+    const { name, isLoading, redirect } = this.state;
+
     return (
       <div data-testid="page-login">
-        Primeiro requisito
+        {
+          isLoading
+            ? <Loading />
+            : (
+              <form>
+                <input
+                  type="text"
+                  name="name"
+                  value={ name }
+                  onChange={ this.handleChange }
+                  data-testid="login-name-input"
+                  placeholder="Digite seu nome"
+                />
+                <button
+                  type="button"
+                  data-testid="login-submit-button"
+                  onClick={ this.submitButton }
+                  disabled={ name.length < MAX_NAME_LENGTH }
+                >
+                  Enter
+                </button>
+              </form>
+            )
+        }
+        {
+          redirect ? <Redirect to="search" /> : ''
+        }
       </div>
     );
   }
