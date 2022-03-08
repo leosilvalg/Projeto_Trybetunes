d4564994f3c2d3b386d0dafb1d4acc5d8e378631
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-03-08 00:09:35 -0300

    Requisito 05. Criando o campo de busca

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import Header from '../Components/Header';
 
+const MAX_NAME_ARTIST_LENGTH = 2;
+
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({ name: value });
+  }
+
   render() {
+    const { name } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Primeiro requisito
+        <form>
+          <input
+            type="text"
+            name="name"
+            value={ name }
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+            placeholder="Digite o nome do artista"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ name.length < MAX_NAME_ARTIST_LENGTH }
+          >
+            Search
+          </button>
+        </form>
       </div>
     );
   }
